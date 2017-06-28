12422208e5f297c2da6907aa37c647acbd727811
Kate F <kate@elide.org>
2017-06-28 14:09:57 +0100

    Handling for various SJP return states.

--- src/main.c
+++ src/main.c
@@ -82,22 +82,6 @@ main(void)
 
 	parse(&l, &ast);
 
-	for (;;) {
-		struct sjp_token t;
-
-		r = sjp_lexer_token(&l, &t);
-		if (SJP_ERROR(r)) {
-			fprintf(stderr, "sjp error A: %d, tok->n=%zu\n", r, t.n); /* TODO */
-			exit(EXIT_FAILURE);
-		}
-
-		if (t.type == SJP_TOK_NONE) {
-			break;
-		}
-
-		fprintf(stderr, "r=%d, t=%d '%.*s'\n", r, t.type, (int) t.n, t.value);
-	}
-
 	r = sjp_lexer_close(&l);
 	if (SJP_ERROR(r)) {
 		fprintf(stderr, "sjp error B: %d\n", r); /* TODO */
