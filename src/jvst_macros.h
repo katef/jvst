1016165cde1cb74c9589f70b180bace53ba4fe68
Shannon F. Stewman <shannon.stewman@gmail.com>
2017-07-11 12:50:06 -0400

    Changes code style to be more compliant with katef’s style
    Big ugly commit that tries to strip off the code style bandaid all at
    once.
    
    Conversion initially done with clang-format and fixed up by hand.
    
    Split: 1/2 of original commit 342ba8c5c6f3e1f20e809c06e3fbd9c91856c141

--- src/jvst_macros.h
+++ src/jvst_macros.h
@@ -3,19 +3,20 @@
 
 #define NCOUNTERS 2
 
-#define ARRAYLEN(x) (sizeof (x) / sizeof (x)[0])
-#define STRINGIFY0(x) # x
+#define ARRAYLEN(x) (sizeof(x) / sizeof(x)[0])
+#define STRINGIFY0(x) #x
 #define STRINGIFY(x) STRINGIFY0(x)
-#define SYMCAT0(x,y) x ## y
-#define SYMCAT(x,y) SYMCAT0(x,y)
-#define SYMCAT3(x,y,z) SYMCAT(SYMCAT(x,y),z)
+#define SYMCAT0(x, y) x##y
+#define SYMCAT(x, y) SYMCAT0(x, y)
+#define SYMCAT3(x, y, z) SYMCAT(SYMCAT(x, y), z)
 
 #ifdef static_assert
-#  define STATIC_ASSERT(x,name) static_assert((x),STRINGIFY(name))
+#define STATIC_ASSERT(x, name) static_assert((x), STRINGIFY(name))
 #else
-#  define STATIC_ASSERT(x,name) struct { char tmp[2*(x)-1]; } SYMCAT3(static_assert_, name, __LINE__)
+#define STATIC_ASSERT(x, name)          \
+	struct {                        \
+		char tmp[2 * (x)-1];    \
+	} SYMCAT3(static_assert_, name, __LINE__)
 #endif
 
-
 #endif /* JVST_MACROS_H */
-
--- src/validate.h
+++ src/validate.h
@@ -4,51 +4,56 @@
 #include "sjp_parser.h"
 
 enum jvst_result {
-  JVST_INVALID = -1,
-  JVST_VALID = 0,
-  JVST_MORE = 1,
+	JVST_INVALID = -1,
+	JVST_VALID   = 0,
+	JVST_MORE    = 1,
 
-  // Internal states... should never be returned by
-  // the public API...
-  JVST_NEXT = 2,
+	// Internal states... should never be returned by
+	// the public API...
+	JVST_NEXT = 2,
 };
 
 #define JVST_IS_INVALID(x) ((x) < 0)
 
 struct ast_schema;
 
-enum {
-  JVST_DEFAULT_STACK_SIZE = 1024,
+enum { JVST_DEFAULT_STACK_SIZE = 1024,
 };
 
 struct jvst_state;
 
 /* validation state */
 struct jvst_validator {
-  const struct ast_schema *schema;
+	const struct ast_schema *schema;
 
-  struct jvst_state *sstack;
-  size_t stop;
-  size_t smax;
+	struct jvst_state *sstack;
+	size_t stop;
+	size_t smax;
 
-  struct sjp_parser p;
-  char pstack[4096];
-  char pbuf[4096];
+	struct sjp_parser p;
+	char pstack[4096];
+	char pbuf[4096];
 
-  char kbuf[128]; // object key buffer
+	char kbuf[128]; // object key buffer
 
-  // XXX - revisit this
-  struct {
-    char msg[64];
-    const char *file;
-    int line;
-  } errstack[16];
-  int etop;
+	// XXX - revisit this
+	struct {
+		char msg[64];
+		const char *file;
+		int line;
+	} errstack[16];
+	int etop;
 };
 
-void jvst_validate_init_defaults(struct jvst_validator *v, const struct ast_schema *schema);
-enum jvst_result jvst_validate_more(struct jvst_validator *v, char *data, size_t n);
-enum jvst_result jvst_validate_close(struct jvst_validator *v);
+void
+jvst_validate_init_defaults(struct jvst_validator *v, const struct ast_schema *schema);
+
+enum jvst_result
+jvst_validate_more(struct jvst_validator *v, char *data, size_t n);
+
+enum jvst_result
+jvst_validate_close(struct jvst_validator *v);
 
 #endif /* JVST_VALIDATE_H */
 
+/* vim: set tabstop=8 shiftwidth=8 noexpandtab: */
